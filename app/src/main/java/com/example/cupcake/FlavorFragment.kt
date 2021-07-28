6c0c284912d997e7e4fe376d800cb661af92eadc
Aliaksandr Urbanovich <urbanovichalexandr@mail.ru>
2021-07-28 08:12:27 +0300

    Create a shared ViewModel

--- app/src/main/java/com/example/cupcake/FlavorFragment.kt
+++ app/src/main/java/com/example/cupcake/FlavorFragment.kt
@@ -19,9 +19,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentFlavorBinding
 
